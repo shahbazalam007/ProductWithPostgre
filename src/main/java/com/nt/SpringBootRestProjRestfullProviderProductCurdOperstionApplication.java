512f39e3a669b559758ea7f5e642f82f6ee46e3d
shahbazalam007 <alamshahbaz685@gmail.com>
2023-12-06 02:22:17 +0530

    Initial Commit

---
+++ src/main/java/com/nt/SpringBootRestProjRestfullProviderProductCurdOperstionApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestProjRestfullProviderProductCurdOperstionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestProjRestfullProviderProductCurdOperstionApplication.class, args);
+	}
+
+}
